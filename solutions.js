061fbe76c687da45466693180bcbc00e4b7dc3ce
KristinaMacias <maciaskristina@gmail.com>
2024-03-19 19:44:25 -0700

    flatten the array and sort

--- solutions.js
+++ solutions.js
@@ -170,3 +170,27 @@ None of the arrays will be empty, so you don't have to worry about that!
 function removeEveryOther(arr){
   return arr.filter((e, i) => i % 2 == 0)
 }
+//Flatten and sort an array
+
+/*
+Challenge:
+
+Given a two-dimensional array of integers, return the flattened version of the array with all the integers in the sorted (ascending) order.
+
+Example:
+
+Given [[3, 2, 1], [4, 6, 5], [], [9, 7, 8]], your function should return [1, 2, 3, 4, 5, 6, 7, 8, 9].
+
+Addendum:
+
+Please, keep in mind, that JavaScript is by default sorting objects alphabetically. For more information, please consult:
+
+http://stackoverflow.com/questions/6093874/why-doesnt-the-sort-function-of-javascript-work-well
+*/
+
+function flattenAndSort(array) {
+  // Good luck, brave code warrior!
+  return array.flat().sort((a, b) => a -b)
+}
+
+console.log(flattenAndSort([[3, 2, 1], [4, 6, 5], [], [9, 7, 8]])); // [1, 2, 3, 4, 5, 6, 7, 8, 9]
