74a34a5449d0a6dc8c9ea52197b6a3ab03338824
KristinaMacias <maciaskristina@gmail.com>
2024-04-24 21:57:30 -0700

    capitalize first letter of every word

--- solutions.js
+++ solutions.js
@@ -560,3 +560,22 @@ function anagrams(stringA, stringB) {
 
 //console.log(anagrams('aba', 'abaa'));
 
+// --- Directions
+// Write a function that accepts a string.  The function should
+// capitalize the first letter of each word in the string then
+// return the capitalized string.
+// --- Examples
+//   capitalize('a short sentence') --> 'A Short Sentence'
+//   capitalize('a lazy fox') --> 'A Lazy Fox'
+//   capitalize('look, it is working!') --> 'Look, It Is Working!'
+
+function capitalize(str) {
+  let wordArr = str.split(' ')
+  console.log(wordArr)
+
+  return wordArr.map((word) => word[0].toUpperCase() + word.substring(1)).join(' ');
+}
+
+module.exports = capitalize;
+
+
